7bcbbf1da1659d0c190a273b3b91435bc4d3d29b
Stefan Latinović <stefan.latinovic@outlook.com>
2024-08-29 16:36:25 +0200

    feat(evm): add initial basic Manager smart contract implementation

---
+++ evm/src/Manager.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import "../src/interfaces/IManager.sol";
+import "../src/interfaces/INFTVault.sol";
+import { CallerNotBridgeMediator } from "../src/ManagerErrors.sol";
+import { MintTokenMessageSent, OnTokenBurnedCallback } from "../src/ManagerEvents.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract Manager is IManager {
+    INFTVault private immutable _nftVault;
+    address private immutable _bridgeMediator;
+
+    modifier onlyBridgeMediator() {
+        if (msg.sender != address(_bridgeMediator)) revert CallerNotBridgeMediator();
+        _;
+    }
+
+    constructor(address nftVault_, address bridgeMediator_) {
+        _nftVault = INFTVault(nftVault_);
+        _bridgeMediator = bridgeMediator_;
+    }
+
+    function deposit(IERC721 collection, uint256 tokenId) external {
+        _nftVault.deposit(collection, tokenId, msg.sender);
+
+        _sendMintTokenMessage(collection, tokenId, msg.sender);
+    }
+
+    function onTokenBurned(IERC721 collection, uint256 tokenId, address owner) external onlyBridgeMediator {
+        _nftVault.release(collection, tokenId, owner);
+
+        emit OnTokenBurnedCallback(address(collection), tokenId, owner);
+    }
+
+    function _sendMintTokenMessage(IERC721 collection, uint256 tokenId, address owner) private {
+        // TODO: Prepare message data
+
+        // TODO: Send message
+
+        emit MintTokenMessageSent(address(collection), tokenId, owner);
+    }
+}
---
+++ evm/test/Manager.t.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.25 <0.9.0;
+
+import { Test } from "forge-std/src/Test.sol";
+
+import { Manager } from "../src/Manager.sol";
+import { NFTVault } from "../src/NFTVault.sol";
+import { NFTExample } from "./NFTExample.sol";
+
+import { CallerNotBridgeMediator } from "../src/ManagerErrors.sol";
+import { MintTokenMessageSent, OnTokenBurnedCallback } from "../src/ManagerEvents.sol";
+
+contract ManagerTest is Test {
+    NFTVault internal nftVault;
+    NFTExample internal collection;
+    Manager internal manager;
+    address internal bridgeMediator = makeAddr("bridgeMediator");
+
+    function setUp() public virtual {
+        nftVault = new NFTVault();
+        collection = new NFTExample();
+        manager = new Manager(address(nftVault), bridgeMediator);
+    }
+
+    function test_deposit_SuccessfulDeposit() external {
+        // Arrange
+        collection.approve(address(nftVault), 1);
+
+        vm.expectEmit(address(manager));
+        emit MintTokenMessageSent(address(collection), 1, address(this));
+
+        // Act
+        manager.deposit(collection, 1);
+
+        // Assert
+        assertEq(collection.ownerOf(1), address(nftVault));
+    }
+
+    function test_onTokenBurned_RevertIf_CallerNotBridgeMediator() external {
+        // Arrange
+
+        // Assert
+        vm.expectRevert(abi.encodeWithSelector(CallerNotBridgeMediator.selector));
+
+        // Act
+        manager.onTokenBurned(collection, 1, address(this));
+    }
+
+    function test_onTokenBurned_SuccessfulRelease() external {
+        // Arrange
+        collection.approve(address(nftVault), 1);
+        vm.expectEmit(address(manager));
+        emit MintTokenMessageSent(address(collection), 1, address(this));
+        manager.deposit(collection, 1);
+        assertEq(collection.ownerOf(1), address(nftVault));
+
+        vm.expectEmit(address(manager));
+        emit OnTokenBurnedCallback(address(collection), 1, address(this));
+
+        // Act
+        vm.prank(bridgeMediator);
+        manager.onTokenBurned(collection, 1, address(this));
+
+        // Assert
+        assertEq(collection.ownerOf(1), address(this));
+    }
+}
