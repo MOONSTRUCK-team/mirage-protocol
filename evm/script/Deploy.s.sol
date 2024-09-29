e3bbfad743d53fd1baafee9df790c62ccf1b6c81
Stefan Latinović <stefan.latinovic@outlook.com>
2024-09-29 20:48:03 +0200

    feat(evm): add basic deploy script

--- evm/script/Deploy.s.sol
+++ evm/script/Deploy.s.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.25 <0.9.0;
 
-import { Foo } from "../src/Foo.sol";
+import { Manager } from "../src/Manager.sol";
+import { NFTVault } from "../src/NFTVault.sol";
+import { BridgeMediator } from "../src/BridgeMediator.sol";
+import { NFTExample } from "../test/NFTExample.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (Foo foo) {
-        foo = new Foo();
+    function run() public broadcast returns (Manager manager) {
+        NFTVault nftVault = new NFTVault();
+        NFTExample _ = new NFTExample();
+        BridgeMediator bridgeMediator = new BridgeMediator(address(0));
+        manager = new Manager(address(nftVault), address(bridgeMediator));
+        bridgeMediator.setManager(address(manager));
     }
 }
